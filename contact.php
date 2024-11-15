e38e4889e1103314bb41fc246c99b4605e7e4349
Nishandh Alec <nishandhalec@gmail.com>
2024-11-15 23:36:04 +0530

    full project

---
+++ contact.php
@@ -0,0 +1,29 @@
+<div class="contact">
+        <div class="contactImg">
+            <img src="pointing.png" alt="Not Found">
+        </div> 
+        <form action="send.php" method="POST">
+            <h2>Get In Touch</h2>
+            <div class="fromAddress">
+                <div class="detail">   
+                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                <p>Address: <span>Puducherry</span></p>
+                </div>
+                <div class="detail">
+                <i class="fas fa-envelope-open" aria-hidden="true"></i>
+                <p>Email Id: <span>nived01@gmail.com</span></p>
+                </div>
+                <div class="detail">
+                <i class="fa fa-phone" aria-hidden="true"></i>
+                <p>Phone No: <span>8304918055</span></p>
+                </div>
+            </div>
+            <input type="text" class="fromField" placeholder="Name" name="name">
+            <input type="email" class="fromField" placeholder="Email Id" name="email">
+            <input type="text" class="fromField" placeholder="Subject" name="subject">
+            <textarea name="feedback" placeholder="Message" class="fromField"></textarea>
+            <div class="fromBtn">
+                <input type="submit" class="fbtn" value="Submit" name="contactSubmit">
+            </div>
+        </form>
+    </div>
